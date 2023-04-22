950b8d0c1f2f66a8d3b7bce36c760e6ac36dee9a
Pushkar Dangi <pushkardangi746@gmail.com>
2023-04-22 11:22:13 +0530

    added env file

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import SearchIcon from "./search.svg";
 
 import MovieCard from "./components/MovieCard";
 
-// get your api key from -> https://omdbapi.com/apikey.aspx
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
-
 const App = () => {
 
   const [movies, setMovies] = useState([]);
@@ -14,7 +11,8 @@ const App = () => {
 
   const searchMovies = async (title) => {
 
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+
     const data = await response.json();
 
     setMovies(data.Search);
